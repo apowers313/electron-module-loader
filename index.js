c203de29c4d137e868becc13f7b22987c351272e
Adam Powers <apowers@ato.ms>
2017-12-18 22:33:34 -0800

    round-trip msg

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 'use strict';
 const electron = require('electron');
-var npm = require("npm");
 
 const app = electron.app;
 
@@ -10,9 +9,27 @@ require('electron-debug')();
 // prevent window being garbage collected
 let mainWindow;
 
+// this chunk of code handles sending messages
+const {ipcMain} = require('electron');
+var sendMsg = null;
+ipcMain.on("send-msg", function(e, msg) {
+	console.log ("ipcMain.on send-msg");
+	// this is synchronous
+	// asynchonous also exists: https://electronjs.org/docs/api/ipc-main
+	var retMsg;
+	if(sendMsg) {
+		retMsg = sendMsg(msg);
+	} else {
+		console.log ("message handler not registered");
+	}
+	e.returnValue = retMsg;
+});
+
+// this chunk of code handles loading modules
+var npm = require("npm");
 global.sharedObj = {};
 global.sharedObj.logger = console.log;
-global.sharedObj.loader = function(path) {
+global.sharedObj.loader = function(path, cb) {
 	console.log("loading module:", path);
 	try {
 		npm.load(function(err) {
@@ -22,13 +39,25 @@ global.sharedObj.loader = function(path) {
 				console.log("successfully installed");
 				console.log("package name:", data[0][0]);
 				console.log("package path:", data[0][1]);
-				require (data[0][1]);
+				var ret = require (data[0][1]);
+				console.log("require done");
+				console.log("require returned:", ret);
+				if (typeof ret.init === "function") {
+					console.log ("initializing npm package...");
+					ret.init();
+				}
+				if (typeof ret.sendMsg === "function") {
+					console.log ("registering sendMsg...");
+					sendMsg = ret.sendMsg;
+					return cb();
+				}
 			});
 
 		})
 	} catch (err) {
 		console.log("couldn't load module");
 		console.log("err:", err);
+		return cb(err);
 	}
 };
 
--- test-package/index.js
+++ test-package/index.js
@@ -1,1 +1,24 @@
-console.log ("PACKAGE!");
+function init() {
+    console.log ("initializing test package");
+}
+
+function shutdown() {
+    console.log ("initializing test package");
+}
+
+function sendMsg(msg) {
+    console.log ("test package sending message:");
+    console.log ("===========================");
+    console.log (msg);
+    console.log ("===========================");
+    console.log ("test package done sending message. open browser console to see return message.");
+    return {
+        pong: "package sez hey!"
+    };
+}
+
+module.exports = {
+    init: init,
+    shutdown: shutdown,
+    sendMsg: sendMsg
+};
